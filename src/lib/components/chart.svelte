ba696bcb2a4a546a71127eaad31d962f207e5812
dianayukari <yukaridiana@gmail.com>
2024-10-18 18:49:31 +0200

    bar chart and temp colors implementes

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,13 @@
+<script>
+    import * as d3 from 'd3';
+
+
+    export let chartHeight
+    export let chartWidth
+    export let data
+
+
+
+    $:console.log(yScale(97))
+
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
 
     import {onMount} from 'svelte';
     import * as d3 from 'd3';
-    import Countdown from '../lib/components/Countdown.svelte';
-
+	import { scale } from 'svelte/transition';
 
     let y = 0
     let countdownNumbers = [];
@@ -11,18 +10,31 @@
     let label = []
 
     let pauseIndices = []
-    let pauseScrolls = 80
+    let pauseScrolls = 60
     let totalScrolls
 
+    let chartWidth
+    let chartHeight
+
+    let data = []
+
     onMount(async() => {
-        const data = await d3.csv("/timeline.csv");
+
+        window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+
+        data = await d3.csv("/timeline.csv");
         countdownNumbers = data.map(d => d.years_ago);
-        label = data.map(d => d.event)
+        label = data.map(d => d.event.replaceAll(";", ";\n"))
         pauseIndices = data.filter(d => d.event !== "NA").map(d => d.years_ago)
 
         totalScrolls = countdownNumbers.length + (pauseIndices.length*pauseScrolls)
 
-    });        
+        chartHeight = window.innerHeight
+        chartWidth = window.innerWidth
+
+    });            
 
     function handleScroll(e) {
         const scrollPct = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
@@ -51,33 +63,81 @@
         } 
 
         currentIndex = Math.min(adjustedIndex - (pauseCount * pauseScrolls), countdownNumbers.length -1)
-
+        
     }
 
+    $: yScale = d3.scaleLinear()
+        .domain([0, 1])
+        .range([chartHeight, 0])
+        
+    $: forestPct = data.map(d => +d.pct)
+    $: column = forestPct[currentIndex]
+
+
+    $: temp = data.map(d => +d.temp)
+    $: bg = temp[currentIndex]
+
+    $: colorScale = d3.scaleLinear()
+        .domain([8, 12.5, 15.4])
+        .range(["#F6F8FF", "#FFFDE4", "#FAB9B9"])
+        .interpolate(d3.interpolate)
+
+    $: console.log(bg);
+    
 
 </script>
 
 <svelte:window on:scroll={handleScroll}/>
 
 <div class="scroll-container" style="--total-scrolls: {totalScrolls}">
-    
+
     <div class="container">
+
+        <p class="temp-label">{bg}</p>
+
+        <div class="bg">
+            <svg width={chartWidth} height={chartHeight}>
+                <rect
+                    x={0}
+                    y={0}
+                    width={chartWidth}
+                    height={chartHeight}
+                    fill={colorScale(bg)}
+                />
+            </svg>
+        </div>
+
         <div class="countdown">
             {#if countdownNumbers[currentIndex]}
                <p class="yearsAgo">{countdownNumbers[currentIndex]} years ago</p>
                 <p class="event">{label[currentIndex]}</p>
             {/if}
         </div>
+
+        <p class="forest-label">{column}</p>
+
+        <div class="chart">
+            <svg width={chartWidth} height={chartHeight}>
+                <rect
+                    x={0}
+                    y={yScale(column)}
+                    width={chartWidth}
+                    height={chartHeight - yScale(column)}
+                    fill="green"
+                />
+            </svg>
+        </div>
+
     </div>
 
-    <div class="chart"></div>
+
 
 </div>
 
 <style>
 
     .scroll-container {
-        height: 8000vh;
+        height: 5000vh;
     }
 
 
@@ -86,19 +146,46 @@
     }
 
     .event {
-        /* text-align: center; */
+        white-space: pre-line
+    }
+
+    .bg {
+        position: absolute;
+        /* display: inline; */
     }
 
     .container {
-        width: 200px;
+        width: 100vw;
         height: 100vh;
         display: flex;
         flex-direction: column;
         align-items: center;
         position: fixed;
-        top: 50%;
-        left: calc(50% - 100px);
         right: 0;
+        left: 0;
+    }
+
+    .temp-label {
+        position: absolute;
+        z-index: 2;
+    }
+
+    .forest-label {
+        z-index: 2;
+        position: absolute;
+        top: 90vh;
+    }
+
+    .countdown {
+        width: 180px;
+        position: fixed;
+        top: 50%;
+        left: calc(50% - 90px);
+        z-index: 2;
+    }
+
+    .chart {
+        z-index: 1;
     }
 
 
